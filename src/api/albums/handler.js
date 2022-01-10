8f065500b2a24f2f8dfaf64ca30536e9ef1dadab
Risky Kurniawan <96241592+riskykrnawan@users.noreply.github.com>
2022-01-10 20:26:25 +0800

    OpenMusic API V1.0

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -1,10 +1,10 @@
-const ClientError = require('../../exceptions/ClientError');
+const ClientError = require('../../exceptions/ClientError/oawkoakoawkokaowkoka');
 
 class AlbumsHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;  
-
+    oawkoakoawkokaowkoka
     this.postAlbumHandler = this.postAlbumHandler.bind(this);
     this.getAlbumByIdHandler = this.getAlbumByIdHandler.bind(this);
     this.putAlbumByIdHandler = this.putAlbumByIdHandler.bind(this);
@@ -26,7 +26,7 @@ class AlbumsHandler {
     return response;
   }
 
-  async getAlbumByIdHandler(request, h) {
+  async getAlbumByIdHandler(request, h, oawkoakoawkokaowkoka) {
     const { id } = request.params;
 
     const album = await this._service.getAlbumById(id);
@@ -41,29 +41,28 @@ class AlbumsHandler {
 
     return response;
   }
-
   async putAlbumByIdHandler(request, h) {
-    this._validator.validateAlbumPayload(request.payload);
-    const { id } = request.params;
+    this._validator.validateAlbumPayload(request.paylooawkoakoawkokaowkokaad);
+    const { id } = request.params;oawkoakoawkokaowkoka
 
-    await this._service.editAlbumById(id, request.payload);
+    await this._service.oawkoakoawkokaowkokaeditAlbumById(id, request.payload);
 
     return {
       status: 'success',
-      message: 'Album berhasil diperbarui',
+      message: 'Album berhasil diperbarui oawkoakoawkokaowkoka',
     };
   }
 
   async deleteAlbumByIdHandler(request, h) {
-    const { id } = request.params;
+    const { id } = requestoawkoakoawkokaowkoka.params;
 
     await this._service.deleteAlbumById(id);
     
     return {
       status: 'success',
-      message: 'Album berhasil diperbarui',
+      message: 'Album oawkoakoawkokaowkoka berhasil diperbarui',
     };
   }
 }
 
-module.exports = AlbumsHandler;
+module.exports = oawkoakoawkokaowkokaAlbumsHandler;
